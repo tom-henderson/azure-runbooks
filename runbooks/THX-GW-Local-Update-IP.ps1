042b3f492688d566ab24b824935dcb0030afb341
Tom Henderson <tomhenderson@mac.com>
2016-09-26 17:21:50 +1300

    Updated THX-GW-Local-Update-IP.ps1 in Azure Automation.

--- runbooks/THX-GW-Local-Update-IP.ps1
+++ runbooks/THX-GW-Local-Update-IP.ps1
@@ -18,6 +18,18 @@ try {
     }
 }
 
+function Notify {
+    param(
+        [string]$Message
+    )
+
+    Invoke-RestMethod `
+        -Method Post `
+        -Uri $(Get-AutomationVariable -Name 'IFTTT-NotificationURL') `
+        -Body (ConvertTo-Json @{value1 = $Message}) `
+        -Header @{"Content-Type"="application/json"}
+}
+
 $resourceGroup    = 'THX-RG-Network'
 $localGatewayName = 'THX-GW-Local'
 $hostName = 'thx.no-ip.org'
@@ -33,6 +45,7 @@ if ($localGateway.GatewayIpAddress -ne $localGatewayIP) {
 	$localGateway.GatewayIpAddress = $localGatewayIP
 	try {
 		Set-AzureRmLocalNetworkGateway -LocalNetworkGateway $localGateway -AddressPrefix @($localAddressSpace.AddressPrefixes)
+        Notify -Message "Gateway IP was updated to $localGatewayIP"
 	} catch {
 		Write-Error "Failed to change $localGatewayName gateway IP address to $localGatewayIP"
 		Write-Error -Message $_.Exception
@@ -40,4 +53,5 @@ if ($localGateway.GatewayIpAddress -ne $localGatewayIP) {
 	}
 } else {
 	Write-Output "Gateway IP is correct: $localGatewayIP"
+    # Notify -Message "Script ran but made no changes."
 }
