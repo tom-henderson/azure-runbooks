5d300f15e94a060f7019c7d87111e01457be29c9
Tom Henderson <tomhenderson@mac.com>
2016-09-26 16:20:03 +1300

    Updated THX-Send-Notification.ps1 in Azure Automation.

--- runbooks/THX-Send-Notification.ps1
+++ runbooks/THX-Send-Notification.ps1
@@ -2,11 +2,32 @@ param (
     [object]$WebhookData
 )
 
+$connectionName = "AzureRunAsConnection"
+try {
+    # Get the connection "AzureRunAsConnection"
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+} catch {
+    if (!$servicePrincipalConnection) {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
 
 if ($WebhookData -ne $null) {
     $WebhookBody = ConvertFrom-Json -InputObject $WebhookData.RequestBody
     $Message = $WebhookBody.Message
+    $Endpoint = Get-AzureAutomationVariable -AutomationAccountName "THX-Automation" -Name "IFTTT-NotificationURL"
     Write-Output $Message
+    Write-Output $Endpoint.value
 } else {
     Write-Error "Must be called via webhook."
 }
